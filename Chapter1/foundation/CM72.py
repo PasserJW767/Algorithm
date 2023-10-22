aad7d32a782d203f6751a6c0b4702448041d69a1
LiutongJiang <709263897@qq.com>
2023-10-22 18:12:30 +0800

    Update CM72

--- Chapter1/foundation/CM72.py
+++ Chapter1/foundation/CM72.py
@@ -8,6 +8,7 @@
 '''
 
 class UnusualAdd:
+    # 我的解法
     def addAB(self, A, B):
         # write code here
         A_2 = bin(A)[2:]
@@ -16,15 +17,9 @@ class UnusualAdd:
         flag = False
         # 对齐A_2和B_2
         if len(A_2) < len(B_2):
-            zero_str = ''
-            for i in range(abs(len(A_2) - len(B_2))):
-                zero_str += '0'
-            A_2 = zero_str + A_2
+            A_2 = "0" * abs(len(A_2) - len(B_2)) + A_2
         elif len(B_2) < len(A_2):
-            zero_str = ''
-            for i in range(abs(len(A_2) - len(B_2))):
-                zero_str += '0'
-            B_2 = zero_str + B_2
+            B_2 = "0" * abs(len(A_2) - len(B_2)) + B_2
         # 根据不同的情况进行相加，用flag标志进位
         for i in range(len(A_2) - 1, -1, -1):
             if (A_2[i] == '1' and B_2[i] == '0') or (B_2[i] == '1' and A_2[i] == '0'):
@@ -48,7 +43,30 @@ class UnusualAdd:
             result = '1' + result
         return int(result, base=2)
 
-            
+    # 别人的解法 
+    '''
+    试想二进制0101和1101的相加过程
+    0 1 0 1
+    1 1 0 1
+    其实可以看成是不带进位的结果1000和进位产生的1010相加。
+
+    而“不带进位的加法”其实就是异或运算，“进位”其实就是只有两个1的时候才会出现，
+    也就是与运算，只是因为进了一位，所以还要往左移动一位。
+
+    这样就将两个数相加通过位运算转换成了不带进位的加法结果和进位相加的问题，
+    反复进行下去，最后只要没有进位了，相加的结果也就得到了。
+    '''
+    def addAB2(self, A, B):
+        print(A)
+        print(B)
+        while(B!=0) :
+            t = A^B
+            print(A&B)
+            B = (A&B)<<1
+            A = t
+            print(A)
+            print(B)
+        return A
 
 if __name__ == '__main__':
     UnusualAdd().addAB(16, 64)
